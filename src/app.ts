df6a6d0c63c0b47d5fa1d3961f1de9e6497483c0
renangiaretta <ranan.giaretta@hotmail.com>
2023-01-30 21:20:55 -0300

    cleaning tests

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,59 @@
-import express, { Application } from 'express'
-import { consoleLogMiddleware, consoleLogTestMiddleware, createWorkOrder, listWorkOrder } from './logic'
-
+import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 app.use(express.json())
 
-app.post('/work-order',consoleLogMiddleware, consoleLogTestMiddleware, createWorkOrder)
-app.get('/work-order', listWorkOrder)
+//Interfaces
+interface IListRequest {
+    listName: string
+    data: data
+    id: number
+}
 
+interface data {
+    name: string
+    quantity: string
+}
 
-app.listen(3000, () =>{
+// interface IList extends IListRequest{
+//     id: number
+// }
+
+// database
+let lists: Array<IListRequest> = []
+
+// logic
+let startId: number = 0
+function generateId () {
+    return startId++
+}
+// create new list
+app.post('/purchaseList', (request: Request, response: Response): Response => {
+
+    const listData: IListRequest = request.body
+    const newListData: IListRequest = {
+        ...listData,
+        id: generateId()
+    }
+    lists.push(newListData)
+    return response.status(201).json(newListData)
+})
+
+// find all lists
+app.get('/purchaseList', ( request: Request, response: Response ): Response => {
+    // const allLists = lists
+    return response.status(200).json(lists)
+})
+
+// find an list by id 
+app.get(`/purchaseList/:id`, (request: Request, response: Response): Response => {
+    const id: number = parseInt(request.params.id)
+    console.log(id)
+    const indexList = lists.findIndex(el => el.id === id)
+    console.log(indexList)
+    return response.status(200).json(lists[indexList])
+})
+
+app.listen(3000, () => {
     console.log('Server is online.')
 })
--- src/database.ts
+++
@@ -1,7 +0,0 @@
-import { IWorkOrder } from "./interfaces"
-
-
-let orders: Array<IWorkOrder> = []
-let ids: Array<number>        = []
-
-export { orders, ids }
